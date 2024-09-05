80005c297d432970ffdbe8e5fb8ea291cd580781
339741886 <339741886@2260I-L29-008>
2024-09-05 09:29:14 -0400

    Add project files.

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Ashton"); // This prints my name
+            Console.WriteLine("");
+            Console.WriteLine("Comments are used to leave notes about your program");
+            Console.WriteLine("");
+            Console.WriteLine("   A     H    H");
+            Console.WriteLine("  A A    H    H");
+            Console.WriteLine(" A   A   HHHHHH");
+            Console.WriteLine("AAAAAAA  H    H");
+            Console.WriteLine("A     A  H    H");
+
+            // This is a comment
+
+            /* This is a 
+             * multi-line
+             * comment */
+        }
+    }
+}
